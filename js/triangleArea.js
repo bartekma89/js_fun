f074ec50c2061a28b1d0794e483d27eab2cf1b41
Bartek <bartekma89@gmail.com>
2017-10-21 13:09:19 +0200

    fixed triangleArea bugs

--- js/triangleArea.js
+++ js/triangleArea.js
@@ -1,5 +1,16 @@
 var a = prompt('Base value a: ');
+if (a <= 0) {
+	alert('Wartość musi być dodatnia');
+	a = prompt('Base value a: ');
+}
+
 var h = prompt('Height value h: ');
-var triangleArea = a*h/2;
+if (h <= 0) {
+	alert('Wartość musi byc dodatnia');
+	h = prompt('Height value h: ');
+}
+//calculate
+var triangleArea = a * h / 2;
+//statements
 alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is egual to: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
